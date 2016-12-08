c051f06f54e8cf40331aea6bd0155f4a94b58135
王会洲 <wanghuizhou@uknower.com>
2016-12-08 15:58:34 +0800

    工程代码

---
+++ cHeathKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cHeathKit
+//
+//  Created by 王会洲 on 16/12/8.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cHeathKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cHeathKit
+//
+//  Created by 王会洲 on 16/12/8.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
